9b42bce3dded51752e7bf96fd2845a56ef176073
Irene Testini <48917143+irene-crepax@users.noreply.github.com>
2024-01-22 13:43:52 +0000

    Update pipeline.py
    argument added for num_threads

--- pipeline.py
+++ pipeline.py
@@ -12,12 +12,15 @@ parser.add_argument('--type_of_cues', type=str,
                     help='type of cues to train NDL')
 parser.add_argument('--size_of_sample', type=int, default=0,
                     help='size of sample from Araneum')
+parser.add_argument('--num_threads', type=int, default=4,
+                    help='number of threads')
 
 args = parser.parse_args()
 dataset_type = args.stratification
 araneum = args.path_to_local_corpus
 cues_type = args.type_of_cues
 N = args.size_of_sample
+T = args.num_threads
 
 
 ### UNCOMMENT THE STEPS YOU ARE INTERESTED IN RUNNING. BE AWARE THAT EACH STEP RELIES ON ALL THE PREVIOUS ONES ####
@@ -26,7 +29,7 @@ N = args.size_of_sample
 #Step03_find_reflexives.tag_reflexives()
 #Step04_prepare_corpus.prepare(N)
 #Step05_extract_lemmas.extract_lemmas()
-#Step06_extract_ngrams.extract_ngrams()
+#Step06_extract_ngrams.extract_ngrams(T)
 #Step07_prepare_cues_to_use.prepare_all_cues()
 #Step08_split.splitData(dataset_type)
 #Step09_make_eventfiles.make_eventfiles(dataset_type)
